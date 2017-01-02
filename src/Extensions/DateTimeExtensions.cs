d9a6c545a4348b8379aa8cb90a3020bcad9e723f
Tre`Von McKay <trevon.mckay@gmail.com>
2017-01-02 10:35:11 -0800

    Rewrote to support unix seconds and milliseconds

--- src/Extensions/DateTimeExtensions.cs
+++ src/Extensions/DateTimeExtensions.cs
@@ -4,10 +4,34 @@ namespace Wavy.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static long ToUnixTime(this DateTime instance)
+        /// <summary>
+        /// Converts the current datetime value to unix time in seconds.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public static long ToUnixTimeSeconds(this DateTime instance)
         {
-            var timeSpan = (instance.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0));
-			return (long)timeSpan.TotalSeconds;
+            var unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var utcDate = instance.ToUniversalTime();
+            var ticks = utcDate.Ticks - unixEpoch.Ticks;
+            var timeSpan = ticks / TimeSpan.TicksPerSecond;
+
+            return timeSpan;
+        }
+
+        /// <summary>
+        /// Converts the current datetime value to unix time in milliseconds.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public static long ToUnixTimeMilliseconds(this DateTime instance)
+        {
+            var unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var utcDate = instance.ToUniversalTime();
+            var ticks = utcDate.Ticks - unixEpoch.Ticks;
+            var timeSpan = ticks / TimeSpan.TicksPerMillisecond;
+
+            return timeSpan;
         }
     }
 }
