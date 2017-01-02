75c0e65e646803d58adceb82ae198b7ac621cc18
Tre`Von McKay <trevon.mckay@gmail.com>
2017-01-02 11:03:16 -0800

    Added more unit tests

--- test/Helpers.Tests/DateTimeHelperTest.cs
+++ test/Helpers.Tests/DateTimeHelperTest.cs
@@ -6,13 +6,39 @@ namespace Wavy.Helpers.Tests
     public class DateTimeHelperTest
     {
         [Fact]
-        public void InputZeroShouldReturnUnixEpoch()
+        public void CreateDateTimeFromUnixSecondsZeroShouldReturnUnixEpoch()
         {
             //Given
             var expected = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         
             //When
             var result = DateTimeHelper.CreateDateTimeFromUnixSeconds(0);
+        
+            //Then
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CreateDateTimeFromUnixMillisecondsZeroShouldReturnUnixEpoch()
+        {
+            //Given
+            var expected = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        
+            //When
+            var result = DateTimeHelper.CreateDateTimeFromUnixMilliseconds(0);
+        
+            //Then
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CreateDateTimeFromUnixSecondsShouldReturnFeb142005()
+        {
+            //Given
+            var expected = new DateTime(2005, 2, 14, 0, 0, 0, DateTimeKind.Utc);
+
+            //When
+            var result = DateTimeHelper.CreateDateTimeFromUnixSeconds(1108339200);
         
             //Then
             Assert.Equal(expected, result);
